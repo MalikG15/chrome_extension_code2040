b7e597355575e2b6ee4991a3881e56a3129eeab3
Malik Graham <malikgraham15@yahoo.com>
2016-08-20 17:45:16 -0400

    Added functionality to show status in extension window

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Chrome_Extension_CODE2040
 
 This chrome extension connects to CODE2040's API. It bypasses Chrome's CORS policy due to the fact that is it an extension, and in the manifest file CODE2040's API has been declared as a permissible access point.
+
+# .gitignore
+
+The .gitignore file contains my private token key to connect to the API.
--- popup.js
+++ popup.js
@@ -12,15 +12,42 @@
 	}
 });*/
 
-var xml = new XMLHttpRequest();
-var url = 'http://challenge.code2040.org/api/register';
+function register() {
+	var xml = new XMLHttpRequest();
+	var url = 'http://challenge.code2040.org/api/register';
 
-xml.onreadystatechange = function() {
-    if (xml.readyState == 4 && xml.status == 200) {
-        $("#success").append("<p>You have connected to CODE2040's API and registered</p>");	
-    }
-};
+	xml.onreadystatechange = function() {
+	    if (xml.readyState == 4 && xml.status == 200) {
+	        $("#success").append("<p>You have connected to CODE2040's API and registered</p>");	
+	    }
+	};
 
-xml.open("POST", url);
-xml.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-xml.send(JSON.stringify({"token": token_key.token, "github": "https://github.com/MalikG15/chrome_extension_code2040"}));
+	xml.open("POST", url);
+	xml.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+	xml.send(JSON.stringify({"token": token_key.token, "github": "https://github.com/MalikG15/chrome_extension_code2040"}));
+}
+
+function reverseString(string) {
+	var reversedString = "";
+	for (var x = string.length() - 1; x > 0 ; x--) {
+		reverseString += string.charAt(x);
+	}
+	return reverseString;
+}
+
+function reverseString() {
+	var xml = new XMLHttpRequest();
+	var url = 'http://challenge.code2040.org/api/reverse';
+
+	xml.onreadystatechange = function(data) {
+	    if (xml.readyState == 4 && xml.status == 200) {
+	        $("#success").append("<p> The word given by CODE2040 is " + xml.responseText + "</p>");	
+	    }
+	};
+
+	xml.open("POST", url, false);
+	xml.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+	xml.send(JSON.stringify({"token": token_key.token}));
+}
+
+reverseString();	
