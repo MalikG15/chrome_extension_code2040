68e7ef4f3e1d8a89dea6aa40d5928e12a695846e
Malik Graham <malikgraham15@yahoo.com>
2016-08-20 17:19:43 -0400

    Registered to CODE2040 API

---
+++ popup.js
@@ -0,0 +1,27 @@
+/*$.ajax({
+	method: 'POST',
+	crossDomain: true,
+	url:  'http://challenge.code2040.org/api/register',
+	dataType: 'JSON',
+	data: JSON.stringify({"token": "c566e0e36eff49e500e7926d2fa0180c", "github": "https://github.com/MalikG15/CODE2040"}),
+	success: function(data) {
+		$("#success").append("<p>You have connected to CODE2040's API</p>");
+	},
+	error: function(xhr, status) {
+		$("#success").append(status);
+	}
+});*/
+
+var xml = new XMLHttpRequest();
+var url = 'http://challenge.code2040.org/api/register';
+
+xml.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    	alert("hello");
+        $("#success").append("<p>You have connected to CODE2040's API</p>");	
+    }
+};
+
+xml.open("POST", url);
+xml.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+xml.send(JSON.stringify({"token": token_key.token, "github": "https://github.com/MalikG15/CODE2040_1"}));
